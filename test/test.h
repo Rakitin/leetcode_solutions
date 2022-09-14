be1627683ca55a7a70e18d887d8a595838ebba63
Dmitry Rakitin <dimrakitin@yandex.ru>
2022-09-14 11:42:47 +0300

    added time to test

--- test/test.h
+++ test/test.h
@@ -2,6 +2,8 @@
 
 #include <exception>
 #include <iostream>
+#include <iomanip>
+#include <chrono>
 
 void assert_that(bool statement, const char *message)
 {
@@ -13,8 +15,12 @@ void assert_that(bool statement, const char *message)
 void run_test(void (*test_fun)(), const char *name)
 {
     try {
+        std::chrono::system_clock clock;
+        auto start = clock.now();
         test_fun();
-        std::cout << "[+] Test [" << name << "] successful" << std::endl;
+        std::chrono::duration<double> timeDiff = clock.now() - start;
+        std::cout << std::fixed << std::setprecision(10) << std::left;
+        std::cout << "[+] Test [" << name << "] successful [time: "<< timeDiff.count() << "]" << std::endl;
     } catch (const std::exception &ex) {
         std::cout << "[-] Test failure in [" << name << "]. " << ex.what() << std::endl;
     }
