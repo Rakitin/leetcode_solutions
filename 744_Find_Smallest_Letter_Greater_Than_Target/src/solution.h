e6baac9d59c585d10bd54906ab487a746662de4b
Dmitry Rakitin <dimrakitin@yandex.ru>
2022-09-20 10:34:59 +0300

    edited solv

--- 744_Find_Smallest_Letter_Greater_Than_Target/src/solution.h
+++ 744_Find_Smallest_Letter_Greater_Than_Target/src/solution.h
@@ -6,12 +6,21 @@
 class Solution {
 public:
     char nextGreatestLetter(std::vector<char>& letters, char target) {
-        char result = letters[0];
-        for (char c: letters) {
-            if (c > target) {
-                return c;
+        int l = 0;
+        int r = letters.size() - 1;
+        if (letters[r] <= target) {
+            return letters[0];
+        }
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (letters[m] > target) {
+                r = m - 1;
+            }
+            else {
+                l = m + 1;
             }
         }
-        return result;
+        return letters[l];
     }
 };
